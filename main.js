cb18c967e914fde10dd45ab016458f6418e80d05
Jeff Pancerz <91769428+jspancerz@users.noreply.github.com>
2023-01-12 22:58:50 -0600

    added data attributes and click event listeners

--- main.js
+++ main.js
@@ -5,27 +5,32 @@ const gameboard = ( () => {
     
     //gameboard where players can click to place their marker
     const gameboard = [ 0, 1, 2, 3, 4, 5, 6, 7, 8 ]
-    
-    //helper function to create a single square of the gameboard grid.
-    const createSquare = () => {
+
+    //helper function to create a single square of the gameboard grid & assign it an index for refernce.
+    const createSquare = (index) => { 
         const div = document.createElement( 'div' )
-        const gamegrid = document.getElementById('gamegrid')
+        const gamegrid = document.getElementById( 'gamegrid' )
         div.classList.add( 'square' )
-        gamegrid.appendChild(div)
-    }
+        div.setAttribute( 'index', index )
+        gamegrid.appendChild( div )
+        //this part needs to take into account the players marker, add it to the correlating index of the array, and lock it in so another player cant overwrite their move.
+        div.addEventListener('click', () => console.log(`you clicked on square # ${index}`))
+    } 
+
     //closure function that creates the gameboard
-    const generateGrid = () => gameboard.forEach( Element => createSquare() )
+    const generateGrid = () => gameboard.forEach(( Element, index ) => createSquare(index))  
+    
     generateGrid()
     return {gameboard};
 } )();
 
 
 //this factory function creates an instance of a player with user defined data. 
-const newPlayer = ( name, marker ) => {
-    const placeMarker = () => console.log('i place either x or o here.')
+// const newPlayer = ( name, marker ) => {
+//     const placeMarker = () => console.log('i place either x or o here.')
 
-    return { name, marker, placeMarker };
-}
+//     return { name, marker, placeMarker };
+// }
 
 
 
