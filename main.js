2b322c5530f8ab1e93f649b6e129bf33b23ac8bc
Jeff Pancerz <91769428+jspancerz@users.noreply.github.com>
2023-01-23 23:43:14 -0600

    added functionality for a 'space' to be taken

--- main.js
+++ main.js
@@ -1,38 +1,52 @@
+const game = ( () => {
+     //factory that creates both the players representing the two symbols 'x' and 'o'.
+    const newPlayer = ( playerName, symbol ) => {    
+        return { playerName, symbol }
+    }
+    //players
+    const playerOne = newPlayer( 'playerone', 'x' )
+    const playerTwo = newPlayer( 'playertwo', 'o' );
 
-
-//a module IIFE creates the game board in which players can place their markers.
-const gameboard = ( () => {
+    //set the starting player
+    let currentPlayer = playerOne;
     
-    //gameboard where players can click to place their marker
-    const gameboard = [ 0, 1, 2, 3, 4, 5, 6, 7, 8 ]
-
-    //helper function to create a single square of the gameboard grid & assign it an index for refernce.
-    const createSquare = (index) => { 
-        const div = document.createElement( 'div' )
-        const gamegrid = document.getElementById( 'gamegrid' )
-        div.classList.add( 'square' )
-        div.setAttribute( 'index', index )
-        gamegrid.appendChild( div )
-        //this part needs to take into account the players marker, add it to the correlating index of the array, and lock it in so another player cant overwrite their move.
-        div.addEventListener('click', () => console.log(`you clicked on square # ${index}`))
-    } 
-
-    //closure function that creates the gameboard
-    const generateGrid = () => gameboard.forEach(( Element, index ) => createSquare(index))  
-    
-    generateGrid()
-    return {gameboard};
+    //array representing the gameboard.
+    const gameBoard = [ 0, 1, 2, 3, 4, 5, 6, 7, 8 ]
+  
+    //available to global scope
+    return {gameBoard, playerOne, playerTwo, currentPlayer}
 } )();
 
+//display controller module stores all the visual/interactive elements of the codebase.
+const displayController = ( () => {
+    const grid = document.querySelector( '#gamecontainer' )
+    const selectBox = document.querySelectorAll( '.square' )
+    const newGameBtn = document.querySelector('#new-game')
+        
+    //function to place symbol on gameboard (x or o)
+    const placeSymbol = ( event ) => {
+        if ( event.target.innerText == '' ) {
+            event.target.innerText = game.currentPlayer.symbol;
+            game.currentPlayer = game.currentPlayer === game.playerOne ? game.playerTwo : game.playerOne;
+        } else {
+            null
+        }
+    }
+    //creates single box of the gameboard grid.
+    const createBox = ( index ) => {
+        const box = document.createElement( 'div' )
+        box.classList.add( 'square' )
+        grid.appendChild( box )
+        box.addEventListener('click', placeSymbol)
+    }
 
-//this factory function creates an instance of a player with user defined data. 
-// const newPlayer = ( name, marker ) => {
-//     const placeMarker = () => console.log('i place either x or o here.')
+    //creates the visual representation of the gameboard inside the dom.
+    const populateGameBoard = () => game.gameBoard.forEach( ( element, index ) => createBox() )
 
-//     return { name, marker, placeMarker };
-// }
+    
+    populateGameBoard()
+    //returns to global scope
+    return{populateGameBoard}
+} )()
 
-
-
-
-
+//use formdata again to grab user name and x/y prefernce to feed the factory function parameters and create the newPlayer object.
