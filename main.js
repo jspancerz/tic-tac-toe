470e8ae91b2ef9d3e2e578d5d73282fdd6868041
Jeff Pancerz <91769428+jspancerz@users.noreply.github.com>
2023-01-24 23:59:26 -0600

    comments about how to create next part of project.

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ const game = ( () => {
     let currentPlayer = playerOne;
     
     //array representing the gameboard.
-    const gameBoard = [ 0, 1, 2, 3, 4, 5, 6, 7, 8 ]
+    const gameBoard = [ 0, 1, 2, 3, 4, 5, 6, 7, 8]
   
     //available to global scope
     return {gameBoard, playerOne, playerTwo, currentPlayer}
@@ -21,12 +21,18 @@ const game = ( () => {
 const displayController = ( () => {
     const grid = document.querySelector( '#gamecontainer' )
     const selectBox = document.querySelectorAll( '.square' )
-    const newGameBtn = document.querySelector('#new-game')
+    const newGameBtn = document.querySelector( '#new-game' )
+
+    // newGameBtn.addEventListener( 'click', () => {
+    //     console.log('this clicks')
+    // })
         
     //function to place symbol on gameboard (x or o)
     const placeSymbol = ( event ) => {
         if ( event.target.innerText == '' ) {
             event.target.innerText = game.currentPlayer.symbol;
+            console.log(grid.innerText)
+            //code to check if player won/tied the game goes here.
             game.currentPlayer = game.currentPlayer === game.playerOne ? game.playerTwo : game.playerOne;
         } else {
             null
@@ -36,17 +42,28 @@ const displayController = ( () => {
     const createBox = ( index ) => {
         const box = document.createElement( 'div' )
         box.classList.add( 'square' )
+        box.setAttribute( 'location', index)
         grid.appendChild( box )
         box.addEventListener('click', placeSymbol)
     }
 
     //creates the visual representation of the gameboard inside the dom.
-    const populateGameBoard = () => game.gameBoard.forEach( ( element, index ) => createBox() )
+    const populateGameBoard = () => game.gameBoard.forEach(index => createBox(index) )
 
     
     populateGameBoard()
     //returns to global scope
-    return{populateGameBoard}
+    return{populateGameBoard, grid}
 } )()
 
 //use formdata again to grab user name and x/y prefernce to feed the factory function parameters and create the newPlayer object.
+
+//when new game btn is clicked, take current gameboard, use map and create a brand new empty game board in its place and return it.
+
+// //const winOrTie = () => {
+// if ( displayController.grid.innertext ===
+//         )
+// //}
+console.log( typeof ( displayController.grid ) )
+
+//instead of grouping coupling more data together try and create a third module for controlling gameflow, in the module we can switch players turn, check win/tie condition after 'move', and restart/newgame functionality will be here as well?
